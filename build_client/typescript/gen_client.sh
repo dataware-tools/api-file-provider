cfa0b50e5db7909af9b084789cd8c9795f9501ee
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-10-12 12:05:25 +0900

    Add CI for publishing typescript client

---
+++ build_client/typescript/gen_client.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Generate OpenAPI Clients
+#
+
+set -e
+set -x
+
+usage() {
+
+        cat <<EOUSAGE
+-----------------------------------------------------------------
+Usage: $0 <Schema URL> <Exported module name>
+------------------------------------------------------------------
+EOUSAGE
+}
+
+if [ $# -ne 2 ]; then
+        usage
+        exit 1
+fi
+
+URL=$1
+EXPORTED_MODULE_NAME=$2
+
+npm install
+
+curl ${URL} -o ./schema.yaml
+
+npx openapi-typescript-codegen --input ./schema.yaml --useOptions --output ./node/client --client node
+npx openapi-typescript-codegen --input ./schema.yaml --useOptions --output ./browser/client
+
+echo "export * as ${EXPORTED_MODULE_NAME} from \"./client\";" >./browser/index.ts
+echo "export * as ${EXPORTED_MODULE_NAME} from \"./client\";" >./node/index.ts
+
+npm run build
